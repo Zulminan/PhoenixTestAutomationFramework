b7a668aff01ce786eb118fc0e5e3962dc63a6dc2
Zulminan Ahmed <ahmedzulminan@gmail.com>
2025-11-09 13:13:55 +0530

    Completed the Requirement 11
    Created Excel Reader Utility that will read the excel file from the
    testData folder and will be used in the DataProvider component.
    
    Tests have been added in the testng-datadriven.xml

--- src/test/java/com/api/utils/ExcelReaderUtil.java
+++ src/test/java/com/api/utils/ExcelReaderUtil.java
@@ -3,45 +3,39 @@ package com.api.utils;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
 
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ExcelReaderUtil {
+import com.poiji.bind.Poiji;
 
-	public static void main(String[] args) throws IOException {
+public class ExcelReaderUtil {
+	
+	private ExcelReaderUtil()
+	{
 		
-		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("testData"+File.separator+"PhoenixTestData.xlsx");
+	}
+
+	public static <T>Iterator<T> loadTestData(String xlsxFileName,String sheetName, Class<T> clazz) throws IOException 
+	{
 		
+		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(xlsxFileName);
 		
 		XSSFWorkbook myWorkBook = new XSSFWorkbook(is);
 		
-		XSSFSheet mySheet = myWorkBook.getSheet("LoginTestData");
 		
-	   int lastRowIndex = mySheet.getLastRowNum();
+		XSSFSheet mySheet = myWorkBook.getSheet(sheetName);
+		
+		List<T> list = Poiji.fromExcel(mySheet, clazz);
+		
+		
+		return list.iterator();
+		
+		
+	  
 	   
-	   XSSFRow rowHeader = mySheet.getRow(0);
-	   
-	   int lastIndexOfCol = rowHeader.getLastCellNum()-1;
-	   
-	   XSSFRow myRow;
-	   XSSFCell myCell;
-	   
-	   for(int rowIndex=0;rowIndex<=lastRowIndex;rowIndex++)
-	   {
-		   for(int colIndex=0;colIndex<=lastIndexOfCol;colIndex++)
-		   {
-			   myRow = mySheet.getRow(rowIndex);
-			   myCell = myRow.getCell(colIndex);
-			   
-			   System.out.print(myCell+" ");
-			   
-		   }
-		   
-		   System.out.println();
-	   }
 	   
 	  
 		
--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -12,7 +12,7 @@ import com.api.request.model.CreateJobPayload;
 import com.api.request.model.UserCredentials;
 import com.api.utils.CSVReaderUtil;
 import com.api.utils.CreateJobBeanMapper;
-import com.api.utils.ExcelReaderUtil2;
+import com.api.utils.ExcelReaderUtil;
 import com.api.utils.FakerDataGenerator;
 import com.api.utils.JsonReaderUtil;
 import com.dataproviders.api.bean.CreateJobBean;
@@ -75,13 +75,13 @@ public class DataProviderUtils {
 	
 	@DataProvider(name = "LoginAPIExcelDataProvider", parallel = true)
 	public static Iterator<UserBean> loginAPIExcelDataProvider() throws IOException {
-		return ExcelReaderUtil2.loadTestData("testData/PhoenixTestData.xlsx", "LoginTestData", UserBean.class);
+		return ExcelReaderUtil.loadTestData("testData/PhoenixTestData.xlsx", "LoginTestData", UserBean.class);
 	}
 	
 	@DataProvider(name = "CreateJobAPIExcelDataProvider", parallel = true)
 	public static Iterator<CreateJobPayload
 	> createJobAPIExcelDataProvider() throws IOException {
-		Iterator<CreateJobBean> iterator =  ExcelReaderUtil2.loadTestData("testData/PhoenixTestData.xlsx", "CreateJobTestData", CreateJobBean.class);
+		Iterator<CreateJobBean> iterator =  ExcelReaderUtil.loadTestData("testData/PhoenixTestData.xlsx", "CreateJobTestData", CreateJobBean.class);
 		List<CreateJobPayload> payLoadList = new ArrayList<CreateJobPayload>();
 		
 		while(iterator.hasNext())
